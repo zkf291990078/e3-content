286811b79e68632e8b35bd1292d439c7992b0195
zhangkunfeng <291990078@qq.com>
2018-01-17 23:30:18 +0800

    redis 服務類提交

--- e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
+++ e3-content-service/src/main/java/cn/e3mall/content/service/impl/ContentServiceImpl.java
@@ -3,22 +3,22 @@ package cn.e3mall.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.druid.sql.PagerUtils;
-import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
+import cn.e3mall.common.jedis.JedisClient;
 import cn.e3mall.common.pojo.EasyUIDataGridResult;
 import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.JsonUtils;
 import cn.e3mall.content.service.ContentService;
 import cn.e3mall.mapper.TbContentMapper;
 import cn.e3mall.pojo.TbContent;
 import cn.e3mall.pojo.TbContentExample;
-import cn.e3mall.pojo.TbItem;
-import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.pojo.TbContentExample.Criteria;
 
 @Service
@@ -26,6 +26,10 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+	@Value("${CONTENT_LIST}")
+	private String CONTENT_LIST;
 
 	@Override
 	public E3Result addContent(TbContent content) {
@@ -35,6 +39,8 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(new Date());
 		// 插入数据
 		contentMapper.insert(content);
+		//缓存同步,删除缓存中对应的数据。
+		jedisClient.hdel(CONTENT_LIST, content.getCategoryId().toString());
 		return E3Result.ok();
 
 	}
@@ -54,12 +60,29 @@ public class ContentServiceImpl implements ContentService {
 	 */
 	@Override
 	public List<TbContent> getContentListByCid(long cid) {
+		//查询缓存
+		try {
+			String json = jedisClient.hget(CONTENT_LIST, cid + "");
+			//判断json是否为空
+			if (StringUtils.isNotBlank(json)) {
+				//把json转换成list
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		// 设置查询条件
-		criteria.andCategoryIdEqualTo(cid);
+		criteria.andCategoryIdEqualTo(cid); 
 		// 执行查询
 		List<TbContent> list = contentMapper.selectByExampleWithBLOBs(example);
+		try {
+			jedisClient.hset(CONTENT_LIST, cid + "", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return list;
 	}
 
